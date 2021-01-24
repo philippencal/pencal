d868b24bc47dbcdbdc68658f8244615667904454
Philip Pencal <gpencal@hotmail.com>
2021-01-24 18:21:21 +0100

    include number counting for projects

--- javascript/custom.js
+++ javascript/custom.js
@@ -15,3 +15,14 @@ function activeMenuLinkBasedOnSection(linkId, sectionId) {
     $(linkId).addClass('text-secondary');
   }
 }
+
+$('.count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.attr('data-stop') }, {
+    duration: 8000,
+    easing: 'swing',
+    step: function (now) {
+      $this.text(Math.ceil(now));
+    }
+  });
+});
